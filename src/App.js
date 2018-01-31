dac203aad901a7595b7035939eb6e807d9df3a77
Anthony Peruso <perusoa@gmail.com>
2018-01-30 21:07:59 -0500

    Add files that are using Radium for the styles and media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person.js';
 
 class App extends Component {
@@ -55,11 +56,15 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     //Way to enter content conditionally
@@ -79,18 +84,33 @@ class App extends Component {
           }
         </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+          backgroundColor: 'green',
+          color: 'black'
+      }
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working now!</p>
-        <button onClick={this.togglePersonsHandler} style={ style }>Switch Name</button>
-        { persons }
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working now!</p>
+          <button onClick={this.togglePersonsHandler} style={ style }>Switch Name</button>
+          { persons }
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!'));
   }
 }
 
-export default App;
+export default Radium(App);
